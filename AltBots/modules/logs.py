6394a940736fb72050a6177c948ed3c3a06eaf37
@devil82 <153984431+Samyak1222@users.noreply.github.com>
2024-01-23 10:43:26 +0530

    Update logs.py

--- AltBots/modules/logs.py
+++ AltBots/modules/logs.py
@@ -42,7 +42,7 @@ async def logs(legend):
         fetch = await legend.reply(f"__Fetching Logs...__")
     
         with open("AltLogs.txt", "w") as logfile:
-            logfile.write("⚡ ™°‌ 🫧 ☆𝐌𝐎𝐎𝐍𝐘 𝐎𝐏☆ ⚡ [ Bot Logs ]\n\n" + logs)
+            logfile.write("⚡ DEVIL OP ⚡🫧 [ Bot Logs ]\n\n" + logs)
 
         end = datetime.now()
         ms = (end-start).seconds
